37e48bbdbe4b3d71803a35163875d780866be9d5
mehul <mehulbisen1@gmail.com>
2024-06-24 01:04:00 +0530

    test2

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "Demo_instance" {
-    ami = "ami-0dfdc165e7af15242"
+    ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
     user_data = <<-EOF
                 #!/bin/bash
