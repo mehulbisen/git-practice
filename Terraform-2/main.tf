5f33f98798c9fb69b66ca93f28bf7c54263b5fb7
mehul <mehulbisen1@gmail.com>
2025-07-26 17:40:35 +0530

    test-6

--- Terraform-2/main.tf
+++ Terraform-2/main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.subnet_cidr
-    availability_zone = var.availability_zone
+    availability_zone = var.avaibility_zone
 
     tags = {
         Name = "project_vpc"
