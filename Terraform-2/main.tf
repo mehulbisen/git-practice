14cb20c4dc0dccbfc2c6a0fe9f5737235f6a57de
mehul <mehulbisen1@gmail.com>
2025-07-26 17:02:36 +0530

    test-1

---
+++ Terraform-2/main.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+    region = var.region
+}
+
+resource "aws_vpc" "my_vpc" {
+    cidr_block = var.vpc_cidr
+
+    tags = {
+        Name = "project_vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.my_vpc
+    cidr_block = var.subnet_cidr
+
+    tags = {
+        Name = project_vpc
+    }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.my_vpc
+
+    tags = {
+        Name = project_vpc
+    }
+}
+
+resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.my_vpc
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my_igw
+    }
+    tags = {
+        Name = "project_vpc"
+    }
+}
+
+resource "aws_route_table_association" "my_association" {
+    subnet_id = aws_subnet.public_subnet
+    route_table_id = aws_route_table.public_route_table
+}
+
+resource "aws_security_group" "all_tcp" {
+    vpc_id = aws_vpc.my_vpc
+
+    ingress {
+        from_port = "22"
+        to_port = "22"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = "80"
+        to_port = "80"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = "0"
+        to_port = "0"
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "demo-server" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.public_subnet
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.all_tcp]
+}
---
+++ Terraform-2/variable.tf
@@ -0,0 +1,35 @@
+variable "region" {
+    description = "AWS_region_for_resources"
+    type = string
+    default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+    description = "cidr_block_for_vpc"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+    description = "cidr_for_subnet"
+    type = string
+    default = "10.0.1.0/24"
+}
+
+variable "ami_id" {
+    description = "ami_id_for_ec2"
+    type = string
+    default = 
+}
+
+variable "instance_type" {
+    description = "instance_type"
+    type = string
+    default = "t2.micro"
+}
+
+variable "key_name" {
+    description = "Key_pair_for_instance"
+    type = string
+    default = "demo_key"
+}
