7534c9f4a49213457b9868ad3f4e9a5d7b208c4f
mehul <mehulbisen1@gmail.com>
2025-07-27 13:34:49 +0530

    test-7

--- Terraform-3/main.tf
+++ Terraform-3/main.tf
@@ -37,14 +37,14 @@ resource "aws_security_group" "all-tcp" {
         from_port = "22"
         to_port = "22"
         protocol = "tcp"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
         from_port = "0"
         to_port = "0"
         protocol = "-1"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     
 }
